9e45d77b89d335f7f5e5b5d9bc39f12a02aab20a
marLindaT <26019544+marLindaT@users.noreply.github.com>
2019-03-19 19:08:49 -0400

    Delete index_marlindat_JS.js

--- index_marlindat_JS.js
+++
@@ -1,3 +0,0 @@
-
-
-let quoteArr = ["Life can be much broader once you discover one simple fact: Everything around you that you call life was made up by people that were no smarter than you. And you can change it, you can influence it… Once you learn that, you’ll never be the same again. — Steve Jobs"];
